5d762f8232ea11065eed324fa6afa62357da9698
Jim Richardson <weaselkeeper@gmail.com>
2013-09-15 18:52:21 -0700

    clean up and function check

--- src/someproject.py
+++ src/someproject.py
@@ -14,59 +14,35 @@ import sys
 from ConfigParser import SafeConfigParser
 import logging
 
+
+
+def logging_setup():
 # Example conditional import.
-try:
-    from pymongo import Connection
-except ImportError as e:
-    print 'Failed import of pymmongo, system says %s' % e
-    sys.exit(1)
+    try:
+        from pymongo import Connection
+    except ImportError as e:
+        print 'Failed import of pymmongo, system says %s' % e
+        sys.exit(1)
 
-logging.basicConfig(level=logging.WARN,
-                    format='%(asctime)s %(levelname)s - %(message)s',
-                    datefmt='%y.%m.%d %H:%M:%S')
+    logging.basicConfig(level=logging.WARN,
+                        format='%(asctime)s %(levelname)s - %(message)s',
+                        datefmt='%y.%m.%d %H:%M:%S')
 
-
-def run():
     # Setup logging to console.
     console = logging.StreamHandler(sys.stderr)
     console.setLevel(logging.WARN)
     logging.getLogger(PROJECTNAME).addHandler(console)
     log = logging.getLogger(PROJECTNAME)
-
-    ### Set some default variables and constants.
-    CONFIGFILE = os.path.join('/etc', PROJECTNAME,PROJECTNAME +'.conf')
-    return log, CONFIGFILE
-
-def get_config(args,CONFIGFILE):
-    # Now parse the config file.  Get any and all info from config file.
-    parser = SafeConfigParser()
-    configuration = {}
-    if os.path.isfile(CONFIGFILE):
-        config = CONFIGFILE
-    else:
-        log.warn('No config file found at %s' % CONFIGFILE)
-        sys.exit(1)
-    try:
-        if args.repo:
-            repo = args.repo
-        else:
-            repo = parser.get('Repomonger','repo')
-    except:
-        log.warn('config parse failed')
-        sys.exit(1)
-    log.warn('building repo %s' % repo)
-    parser.read(config)
-    return parser
+    return log
 
 
+def run(args,config):
+    ### Do, whatever it is, we do. 
+    log.debug((args,config))
 
-# Here we start if called directly (the usual case.)
-if __name__ == "__main__":
-    """This is where we will begin when called from CLI. No need for argparse
-    unless being called interactively, so import it here"""
 
-    log, CONFIGFILE = run()
-
+def get_options():
+    """ Parse the command line options"""
     import argparse
 
     parser = argparse.ArgumentParser(
@@ -83,14 +59,52 @@ if __name__ == "__main__":
     args = parser.parse_args()
     args.usage = PROJECTNAME + ".py [options]"
 
+    return args
+
+
+def get_config(args):
+    # Now parse the config file.  Get any and all info from config file.
+    parser = SafeConfigParser()
+    configuration = {}
+    CONFIGFILE = os.path.join('/etc', PROJECTNAME,PROJECTNAME +'.conf')
+    if args.config:
+        config = args.config
+    else:
+        if os.path.isfile(CONFIGFILE):
+            config = CONFIGFILE
+        else:
+            log.warn('No config file found at %s' % CONFIGFILE)
+            sys.exit(1)
+
+    parser.read(config)
+
+    try:
+        configuration['SOMEOPTION'] = args.SOMEOPTION
+    except:
+        configuration['SOMEOPTION'] = parser.get('CONFIGSECTION','SOMEOPTION')
+
+    log.warn('Doing things with %s' % configuration['SOMEOPTION'])
+    return configuration
+
+
+
+# Here we start if called directly (the usual case.)
+if __name__ == "__main__":
+    """This is where we will begin when called from CLI. No need for argparse
+    unless being called interactively, so import it here"""
+    args = get_options()
+    log = logging_setup()
 
     if args.debug:
         log.setLevel(logging.DEBUG)
     else:
         log.setLevel(logging.WARN)
-    if args.config:
-        CONFIGFILE = args.config
 
-    _parse_config = get_config(args,CONFIGFILE)
+    # Override config file location if present as option
+    if args.config:
+        config = args.config
+
+    _parse_config = get_config(args)
 
     # and now we can do, whatever it is, we do.
+    run(args,config)
